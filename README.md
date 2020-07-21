d769ed08b8ecbf25a0d6c5b5e7feb056e54c640e
Jakub Horak <horak.kuba@gmail.com>
2020-07-21 21:00:49 +0200

    added executables for all systems, basic readme file, and feature of work with multiple files

---
+++ README.md
@@ -0,0 +1,6 @@
+Install node modules via
+``` 
+npm i
+```
+
+and run executable prepared for selected system. 
--- index.js
+++ index.js
@@ -2,64 +2,76 @@ const fs = require('fs');
 const parser = require('xml2json');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-fs.readFile('./template.xml', (err, data) => {
-  if (err) {
-    if (err.code === 'ENOENT') {
-      console.error("xml file doesn't exist");
-      return;
+fs.readdir('.', (err, files) => {
+  if (err) throw err;
+
+  files.forEach(fileName => {
+    if (fileName.endsWith('.xml')) {
+      convertEdeklaracjeXmlToCsv(fileName);
     }
-
-    throw err;
-  }
-
-  const edeklaracjeJson = parser.toJson(data);
-  const expensesListJson = JSON.parse(edeklaracjeJson)["tns:JPK"]["tns:ZakupWiersz"];
-  const records =
-    expensesListJson.map((expense) => {
-      const newExpense = {}
-      for (prop in expense) {
-        newExpense[prop.slice(4, prop.length)] = expense[prop];
-      }
-
-      return {
-        no: newExpense['LpZakupu'],
-        date: newExpense['DataWplywu'],
-        invoiceId: newExpense['DowodZakupu'],
-        companyName: `NIP: ${newExpense['NrDostawcy']}, ${newExpense['NazwaDostawcy']}`,
-        companyAddress: newExpense['AdresDostawcy'],
-        otherExpenses: newExpense['K_45'],
-        allExpenses: newExpense['K_45'],
-        vat: newExpense['K_46'],
-      }
-    }
-    );
-
-  const cswWriter = createCsvWriter({
-    path: './template.csv',
-    header: [
-      {id: 'no', title: 'no'},
-      {id: 'date', title: 'date'},
-      {id: 'invoiceId', title: 'invoiceId'},
-      {id: 'companyName', title: 'companyName'},
-      {id: 'companyAddress', title: 'companyAddress'},
-      {id: 'description', title: 'description'},
-      '7',
-      '8',
-      '9',
-      '10',
-      'empty',
-      '11',
-      '12',
-      {id: 'otherExpenses', title: 'otherExpenses'},
-      {id: 'allExpenses', title: 'allExpenses'},
-      {id: 'vat', title: 'vat (OPTIONAL)'},
-    ]
   });
-
-  cswWriter.writeRecords(records)
-    .then(() => {
-      console.log('".csv" file saved!');
-    });
 });
 
+function convertEdeklaracjeXmlToCsv(fileName) {
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        console.error("xml file doesn't exist");
+        return;
+      }
 
+      throw err;
+    }
+
+    const edeklaracjeJson = parser.toJson(data);
+    const expensesListJson = JSON.parse(edeklaracjeJson)["tns:JPK"]["tns:ZakupWiersz"];
+    const records =
+      expensesListJson.map((expense) => {
+        const newExpense = {}
+        for (prop in expense) {
+          newExpense[prop.slice(4, prop.length)] = expense[prop];
+        }
+
+        return {
+          no: newExpense['LpZakupu'],
+          date: newExpense['DataWplywu'],
+          invoiceId: newExpense['DowodZakupu'],
+          companyName: `NIP: ${newExpense['NrDostawcy']}, ${newExpense['NazwaDostawcy']}`,
+          companyAddress: newExpense['AdresDostawcy'],
+          otherExpenses: newExpense['K_45'],
+          allExpenses: newExpense['K_45'],
+          vat: newExpense['K_46'],
+        }
+      }
+      );
+
+    const csvFileName = `${fileName.slice(0, fileName.length - 4)}.csv`; 
+
+    const cswWriter = createCsvWriter({
+      path: csvFileName,
+      header: [
+        {id: 'no', title: 'no'},
+        {id: 'date', title: 'date'},
+        {id: 'invoiceId', title: 'invoiceId'},
+        {id: 'companyName', title: 'companyName'},
+        {id: 'companyAddress', title: 'companyAddress'},
+        {id: 'description', title: 'description'},
+        '7',
+        '8',
+        '9',
+        '10',
+        'empty',
+        '11',
+        '12',
+        {id: 'otherExpenses', title: 'otherExpenses'},
+        {id: 'allExpenses', title: 'allExpenses'},
+        {id: 'vat', title: 'vat (OPTIONAL)'},
+      ]
+    });
+
+    cswWriter.writeRecords(records)
+      .then(() => {
+        console.log(`"${csvFileName}" file saved!`);
+      });
+  });
+}
